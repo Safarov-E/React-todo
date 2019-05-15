9495b36c728ce7f71e6a98d94e734919d0145571
Elkhan <elkhan.safarov10@yandex.ru>
2019-05-15 22:50:31 +0500

    Создание макета

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from 'react';  
 import './App.css';
-
+import './index'; 
+ 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return	(
+	  <main>
+	  	<header>
+	  		<h1> React Todo </h1>
+	  	</header>
+
+	  	<section className="todo-list">
+	  		<div className="todo completed">
+	  			<button className="checkbox icon">
+	  				<i className="material-icons">check_box</i>
+	  			</button>
+
+	  			<span className="todo-title"> Изучить JavaScript </span>
+
+	  			<button className="delete icon">
+	  				<i className="material-icons">delete</i>
+	  			</button>
+	  		</div>
+
+	  		<div className="todo">
+	  			<button className="checkbox icon">
+	  				<i className="material-icons">check_box_outline_blank</i>
+	  			</button>
+	  			
+	  			<span className="todo-title"> Изучить React </span>
+
+	  			<button className="delete icon">
+	  				<i className="material-icons">delete</i>
+	  			</button>
+	  		</div>
+	  	</section>
+	  </main>
+	);
 }
 
-export default App;
+export default App
