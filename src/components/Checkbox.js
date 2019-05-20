483b7db26898216f30420c78197c3cb9610632b0
Elkhan <elkhan.safarov10@yandex.ru>
2019-05-20 22:26:20 +0500

    Изминение состояния

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -1,16 +1,25 @@
 import React, {Component} from 'react';   
 
 class Checkbox extends Component {
-	constructor() {
-		super();
+	constructor(props) {
+		super(props);
 
 		this.state = {
-			checked: false
+			checked: this.props.initiallyChecked
 		};
+
+		this.handleClick = this.handleClick.bind(this);
 	}
+
+	handleClick(event) {
+		this.setState({
+			checked: !this.state.checked
+		});
+	}
+
 	render() {
 		return(
-			<button className="checkbox icon">
+			<button className="checkbox icon" onClick={ this.handleClick}>
 					<i className="material-icons">{this.state.checked ? 'check_box' : 'check_box_outline_blank'}</i>
 		  	</button>
 		);
